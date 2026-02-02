67364f036eee06337c01d8363af87cb7b5a3b56f
shengshampoo <shengzh@tutanota.com>
2026-02-02 09:45:19 +0000

    Create build-static-tinc.sh

---
+++ build-static-tinc.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+set -e
+
+WORKSPACE=/tmp/workspace
+mkdir -p $WORKSPACE
+mkdir -p /work/artifact
+
+# tinc vpn
+cd $WORKSPACE
+git clone -b 1.1 https://github.com/gsliepen/tinc
+cd tinc
+export LDFLAGS="-static --static -no-pie -s"
+meson setup builddir
+meson configure -Dprefix=/usr/local/tincmm -Dbuildtype=minsize -Dlzo=enabled -Dlz4=enabled -Dtests=disabled -Ddefault_library=static --strip builddir
+cd builddir
+sed -i 's@.so.3 @.a @g' ./build.ninja
+sed -i 's@.so @.a @g' ./build.ninja
+ninja
+ninja install
+
+cd /usr/local
+tar vcJf ./tincmm.tar.xz tincmm
+
+mv ./tincmm.tar.xz /work/artifact/
